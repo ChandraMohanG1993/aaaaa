bccae8b635d09dc474636006fcb493db911f816d
ChandraMohanG1993 <chandra4ug@gmail.com>
2020-10-22 12:49:02 +0530

    first commit

---
+++ src/app/userrepo/userrepo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userrepo',
+  templateUrl: './userrepo.component.html',
+  styleUrls: ['./userrepo.component.scss']
+})
+export class UserrepoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
